d6f118d7ec5889b9ac756c3589c7d2ad221b4c4c
zipang29 <guillaume.claudic@gmail.com>
2013-02-14 11:13:15 +0100

    Finalisation de la classe de test

--- MachineTri/src/testCProduit.cpp
+++ MachineTri/src/testCProduit.cpp
@@ -5,4 +5,51 @@ int main()
 	CProduit test;
 	test.MakeCode(10, 2);
 	cout << test << endl;
+	
+	cout << "Longueur" << test.getLong() << endl;
+	cout << "Largeur" << test.getLarg() << endl;
+	cout << "Hauteur" << test.getHaut() << endl;
+	cout << "Catégorie" << test.getCateg() << endl;
+	
+	try 
+	{
+		test.setCateg(2);
+		cout << "Test réussi : Définition d'une catégorie ok." << endl;
+	}
+	catch(range_error e)
+	{
+		cout << "Test failed :Définition d'une catégorie pas ok :" << e.what() << endl;
+	}
+	
+	CProduit test2;
+	
+	try
+	{
+		test2.setCateg(602);
+		cout << "Test failed : Borne non respectée" << endl;
+	}
+	catch(range_error e)
+	{
+		cout << "Test réussie" << endl;
+	}
+	
+	try
+	{
+		test2.setCode("test");
+	}
+	catch(length_error e)
+	{
+		cout << "Test failed : " << e.what() << endl;
+	}
+	
+	try
+	{
+		test2.setCode("");
+	}
+	catch(length_error e)
+	{
+		cout << "Test réussi" << endl;
+	}
+	
+	cout << "Code " << test2.getCode() << endl;
 }
