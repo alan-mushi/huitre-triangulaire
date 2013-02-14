4f62d3df0f70ca236ca0e88941c2d7966c7ff711
zipang29 <guillaume.claudic@gmail.com>
2013-02-14 10:10:10 +0100

    Fin de la classe CProduit

--- MachineTri/src/CProduit.cpp
+++ MachineTri/src/CProduit.cpp
@@ -9,14 +9,20 @@ CProduit::CProduit()
 
 void CProduit::MakeCode(int NumProd, int NumPal)
 {
-	string ret = "";	
+	ostringstream tmp;
+	tmp << NumProd << this->getCateg() << NumPal;
+	this->m_code = tmp;
 }
 
-ostream& CProduit::operator(ostream& os, CProduit& UnProd)
+ostream& CProduit::operator<<(ostream& os, CProduit& UnProd)
 {
-
+	os << "Longueur :" << this->m_Long << endl;
+	os << "Largeur :" << this->m_Larg << endl;
+	os << "Hauteur :" << this->m_Haut << endl;
+	os << "Catégorie :" << this->getCateg() << endl;
+	os << "Code :" << this->getCode() << endl;
 }
 CProduit::~CProduit()
 {
-
+	cout << "Adresse du produit détruit" << this << endl;
 }
