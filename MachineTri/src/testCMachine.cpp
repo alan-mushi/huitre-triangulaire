72f2e68cdcb20b35f20140411c2a8f37c2444341
GUITTET Thibault <alan-mushi@yahoo.com>
2013-02-20 13:40:52 +0100

    Fix compilation pour testCMachine + tests plus explicatifs + indent makefile

--- MachineTri/src/testCMachine.cpp
+++ MachineTri/src/testCMachine.cpp
@@ -8,11 +8,15 @@ int main()
 	destination[2] = "Lorient";
 	destination[3] = "Vannes";
 	destination[4] = "Quiberon";
+
+	CMachine *m1;
 	
+	cout << "[*] Test du constructeur" << endl;
+	cout << "\t[*] Test avec paramètres valides ... ";
 	try
 	{
-		CMachine m1(2, 30, 15, destination);
-		cout << "Test réussi." << endl;
+		m1 = new CMachine(2, 30, 15, destination);
+		cout << "Test réussi" << endl;
 	}
 	catch(length_error e)
 	{
@@ -23,6 +27,7 @@ int main()
 		cout << "Test failed : " << e.what() << endl;
 	}
 	
+	cout << "\t[*] Test avec paramètres invalides (Categorie invalide) ... ";
 	try
 	{
 		CMachine m2(-5, 10, 15, destination);
@@ -30,13 +35,14 @@ int main()
 	}
 	catch(length_error e)
 	{
-		cout << "Test réussi." << endl;
+		cout << "Test réussi" << endl;
 	}
 	catch(runtime_error e)
 	{
 		cout << "Test failed : runtime_error émis au lieu de length_error : " << e.what() << endl;
 	}
 	
+	cout << "\t[*] Test avec paramètres invalides (Nombre de produit total invalide) ... ";
 	try
 	{
 		CMachine m3(12, -60, 5, destination);
@@ -51,6 +57,7 @@ int main()
 		cout << "Test failed : runtime_error émis au lieu de length_error : " << e.what() << endl;
 	}
 	
+	cout << "\t[*] Test avec paramètres invalides (Nombre de produits par palette invalide) ... ";
 	try
 	{
 		CMachine m4(2, 30, -5, destination);
@@ -63,28 +70,35 @@ int main()
 	{
 		cout << "Test failed : runtime_error émis au lieu de length_error : " << e.what() << endl;
 	}
+	cout << endl;
 	
-	if(m1.GetNbCateg() == 2)
+	cout << "[*] Test des Getters" << endl;
+	cout << "\t[*] Test de GetNbCateg ... ";
+	if((*m1).GetNbCateg() == 2)
 		cout << "Test réussi" << endl;
 	else
 		cout << "Test Failed : le nombre de catégorie n'est pas correct" << endl;
 		
-	if(m1.GetNbTotalProd() == 30)
+	cout << "\t[*] Test de GetNbTotalProd ... ";
+	if((*m1).GetNbTotalProd() == 30)
 		cout << "Test réussi" << endl;
 	else
 		cout << "Test failed : le nombre total de produit n'est pas correct" << endl;
 		
-	if(m1.GetNbProdParPalette() == 15)
+	cout << "\t[*] Test de GetNbProdParPalette ... ";
+	if((*m1).GetNbProdParPalette() == 15)
 		cout << "Test réussi" << endl;
 	else
-		cout << "Test failed : le nombre de produit par palette n'est pas correct" << end;
+		cout << "Test failed : le nombre de produit par palette n'est pas correct" << endl;
 		
-	if(m1.GetNbTotalPal() == 2)
+	cout << "\t[*] Test de GetNbTotalPal ... ";
+	if((*m1).GetNbTotalPal() == 2)
 		cout << "Test réussi" << endl;
 	else
 		cout << "Test failed : le nombre total de palette n'est pas correct" << endl;
 		
-	if(m1.GetNbPalProduites() == 0)
+	cout << "\t[*] Test de GetNbPalProduites ... ";
+	if((*m1).GetNbPalProduites() == 0)
 		cout << "Test réussi" << endl;
 	else
 		cout << "Test failed : le nombre de palette produite n'est pas correct" << endl;
